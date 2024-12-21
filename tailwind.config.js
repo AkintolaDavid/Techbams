a864de48c0d9ab30235f866baeb351172e2a1c42
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-21 09:02:42 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-// tailwind.config.js
-module.exports = {
+export default {
   content: [
     "./index.html", // Ensure to include this for the HTML
     "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
